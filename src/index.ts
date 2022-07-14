3a6d122288d2dba8db4d9c7b06b712b40a4da620
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-14 11:56:15 -0300

    feat: starting project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { json } from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+
+import router from "./../src/routes/index.js"; 
+import { errorHanddlingMiddleware}  from "./../src/middlewares/errorHandlingMiddleware.js";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(router);
+app.use(errorHanddlingMiddleware);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`)
+})
